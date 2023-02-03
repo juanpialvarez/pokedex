503da24d64a9cf2b0a9ff951451b97923d580d4d
juanpialvarez <73184329+juanpialvarez@users.noreply.github.com>
2023-02-03 14:26:28 -0600

    add text for big pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,9 @@ class Pokemon{
 let pokemon1 = new Pokemon("Bulbasaur", 2.04, ["Grass", "Poison"]);
 let pokemon2 = new Pokemon("Charmander", 2, "Fire");
 let pokemon3 = new Pokemon("Squirtle", 1.08, "Water");
+let pokemon4 = new Pokemon("Charizard", 5.07, ["Fire", "Flying"]);
 
-let pokemonList = [pokemon1, pokemon2, pokemon3];
+let pokemonList = [pokemon1, pokemon2, pokemon3, pokemon4];
 
 console.log(pokemonList);
 
@@ -20,5 +21,5 @@ for (let index = 0; index < pokemonList.length; index++) {
     let className = `#pokemon-name__${index+1}`;
     let classheight = `#height__${index+1}`;
     document.querySelector(className).innerText = pokemonList[index].name;
-    document.querySelector(classheight).innerText = pokemonList[index].height;
+    document.querySelector(classheight).innerText = pokemonList[index].height > 3 ? `${pokemonList[index].height} - That's big`: `${pokemonList[index].height}`;
 }
