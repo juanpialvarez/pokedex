b3a792e9e706bc866ecce47913fa548ce73ed057
juanpialvarez <73184329+juanpialvarez@users.noreply.github.com>
2023-02-03 12:43:27 -0600

    Add loop for for name and height display

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,13 @@ let pokemon1 = new Pokemon("Bulbasaur", 2.04, ["Grass", "Poison"]);
 let pokemon2 = new Pokemon("Charmander", 2, "Fire");
 let pokemon3 = new Pokemon("Squirtle", 1.08, "Water");
 
-let pokemonList = [pokemon1, pokemon2, pokemon3]
+let pokemonList = [pokemon1, pokemon2, pokemon3];
 
-console.log(pokemonList)
+console.log(pokemonList);
+
+for (let index = 0; index < pokemonList.length; index++) {
+    let className = `#pokemon-name__${index+1}`;
+    let classheight = `#height__${index+1}`;
+    document.querySelector(className).innerText = pokemonList[index].name;
+    document.querySelector(classheight).innerText = pokemonList[index].height;
+}
