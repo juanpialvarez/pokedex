cb482477cb66bc7a028a9aa9d881d33bfaec2799
juanpialvarez <73184329+juanpialvarez@users.noreply.github.com>
2023-02-03 18:08:14 -0600

    add 'add pokemon' and 'remove pokemon' buttons

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,6 @@ async function fetchPokemon(pokemon) {
     pokemonImage.id = 'pokemon-image';
     document.getElementById('pokedex-image').appendChild(pokemonImage);
     document.querySelector('#pokemon-name').innerText = pokemonData.name;
-    document.querySelector('#pokemon-name').innerText = pokemonData.name;
     document.querySelector('#height').innerText = pokemonData.height;
     let type = "";
     const pokemonTypes = pokemonData.types;
@@ -17,12 +16,47 @@ async function fetchPokemon(pokemon) {
     }
     document.querySelector('#type').innerText = type;
 }
-document.querySelector('button').addEventListener('click', () => {
+document.querySelector('#button-submit').addEventListener('click', () => {
     let pokemonImage = document.getElementById('pokemon-image') ? document.getElementById('pokemon-image') : false;
     if (pokemonImage) {
-        document.getElementById('pokedex-image').removeChild(pokemonImage)
+        document.getElementById('pokedex-image').removeChild(pokemonImage);
     }
     let pokemon = document.querySelector('#pokemon').value; 
     pokemon = pokemon.toLowerCase();
     fetchPokemon(pokemon);
 })
+
+class Pokemon{
+    constructor(name, height, types){
+        this.name = name;
+        this.height = height;
+        this.types = types;
+    }
+}
+
+let pokemonList = [];
+
+console.log(document.querySelector('#height').textContent);
+
+document.querySelector('#button-add').addEventListener('click', () => { 
+    let newPokemon= new Pokemon( document.querySelector('#pokemon-name').textContent,
+    document.querySelector('#height').textContent,
+    document.querySelector('#type').textContent); 
+    let nameList = [];
+    for (let index = 0; index < pokemonList.length; index++) {
+        nameList.push(pokemonList[index].name)
+    }
+    if (!nameList.includes(newPokemon.name)) {
+        pokemonList.push(newPokemon)
+    }    
+})
+
+document.querySelector('#button-remove').addEventListener('click', () => { 
+    let pokemonName= document.querySelector('#pokemon-name').textContent;
+    let nameList = [];
+    for (let index = 0; index < pokemonList.length; index++) {
+        nameList.push(pokemonList[index].name)
+    }
+    pokemonList.splice(nameList.indexOf(pokemonName), 1) 
+})
+
